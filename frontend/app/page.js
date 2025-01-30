e2cb8dcca19c6f292d925846496f98f379e1ba81
21mebrat <maytotmat@gmail.com>
2025-01-30 11:16:34 +0300

    settingup the project

---
+++ frontend/app/page.js
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const RootHome = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+<Button variant='destructive'>Click me</Button>
+    </div>
+  )
+}
+
+export default RootHome
